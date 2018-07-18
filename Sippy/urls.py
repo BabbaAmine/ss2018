d98d4c6d5d1c024b3d007dba5638ead5635dc7a4
BabbaAmine <amine.babba@sesame.com.tn>
2018-07-19 00:37:26 +0200

    info account ok

--- Sippy/urls.py
+++ Sippy/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^didslist/$', views.didList, name='didsList'),
     url(r'^SDAslist/$', views.SDAList, name='SDAList'),
-    url(r'^customer/(?P<name>[\w\-]+)/$', views.getinfoCustomer, name='infoCustomer')
+    url(r'^customer/(?P<name>[\w\-]+)/$', views.getinfoCustomer, name='infoCustomer'),
+    url(r'^Account/(?P<i_account>[0-9]+)/$', views.getAccountInfo, name='infoAccount')
 ]
--- Sippy/views.py
+++ Sippy/views.py
@@ -5,6 +5,7 @@ import httplib2
 
 def didList(request):
 
+
     class HTTPSDigestAuthTransport:
         def request(self, host, handler, request_body, verbose=0):
 
@@ -29,15 +30,11 @@ def didList(request):
     transport = HTTPSDigestAuthTransport()
     client = ServerProxy("https://ssp-root:123456go@sva.yooth-it.com/xmlapi/xmlapi", transport)
     res = client.getDIDsList({'i_wholesaler': 338})
+
     listdidwithCustomers=[]
     listcustid=[]
     listcustidName=[{'id':int,'name':str}]
     result=res['dids']
-    print(result)
-
-    restestAccount=client.listAccounts({'i_customer': 25})
-    resTest=restestAccount['accounts']
-    #print(resTest)
 
     for item in result:
         if ('delegated_to' in item and item['delegated_to'] != ''):
@@ -111,7 +108,6 @@ def SDAList(request):
     client = ServerProxy("https://ssp-root:123456go@sva.yooth-it.com/xmlapi/xmlapi", transport)
     res = client.getDIDsList({'i_wholesaler': 338})
     result=res['dids']
-    #print(result)
     listSDA=[]
     for r in result:
         if(r['did']!=r['incoming_did']):
@@ -153,6 +149,32 @@ def getinfoCustomer(request,name):
 
     return render(request, 'infoCustomer.html', {'customer':customer })
 
+def getAccountInfo(request,i_account):
 
+    class HTTPSDigestAuthTransport:
+        def request(self, host, handler, request_body, verbose=0):
 
+            h = httplib2.Http()
+            if verbose:
+                h.debuglevel = 1
+            h.add_credentials('ssp-root', '123456go')
+            h.disable_ssl_certificate_validation = True
 
+            resp, content = h.request("https://sva.yooth-it.com/xmlapi/xmlapi" + handler, "POST", body=request_body,
+                                      headers={'content-type': 'text/xml'})
+
+            if resp.status != 200:
+                raise ProtocolError("https://sva.yooth-it.com/xmlapi/xmlapi" + handler,
+                                    resp.status, resp.reason, None)
+
+            p, u = getparser(0)
+            p.feed(content)
+
+            return u.close()
+    id_account=i_account
+    transport = HTTPSDigestAuthTransport()
+    client = ServerProxy("https://ssp-root:123456go@sva.yooth-it.com/xmlapi/xmlapi", transport)
+    resss = client.getAccountInfo({'i_account': id_account })
+    print(resss)
+
+    return render(request, 'InfoAccount.html', {'Account': resss})
